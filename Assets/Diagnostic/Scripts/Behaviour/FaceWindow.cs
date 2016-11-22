5f07c00509299ae8b28af7f4373f72572d5cd1c7
Valentin Macheret <vm@bluefrogrobotics.com>
2016-11-22 18:31:27 +0100

    [APP][DIAG] Add sensors to diagnostic

--- Assets/Diagnostic/Scripts/Behaviour/FaceWindow.cs
+++ Assets/Diagnostic/Scripts/Behaviour/FaceWindow.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 using BuddyOS;
 
 namespace BuddyApp.Diagnostic
 {
     public class FaceWindow : MonoBehaviour
     {
+        [SerializeField]
+        private Dropdown dropdownMood;
+
+        [SerializeField]
+        private Dropdown dropdownEvent;
+
         private Face mFace;
 
         void Start()
@@ -16,12 +23,14 @@ namespace BuddyApp.Diagnostic
             mFace = BYOS.Instance.Face;
         }
 
-        void SetMood()
+        public void SetMood()
         {
+            //mFace.SetMood();
         }
 
-        void SetEvent()
+        public void SetEvent()
         {
+            //mFace.SetEvent((FaceEvent)iEvent);
         }
     }
 }
--- Assets/Diagnostic/Scripts/Behaviour/SensorsWindow.cs
+++ Assets/Diagnostic/Scripts/Behaviour/SensorsWindow.cs
@@ -3,12 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 using BuddyOS;
 
 namespace BuddyApp.Diagnostic
 {
     public class SensorsWindow : MonoBehaviour
     {
+        [SerializeField]
+        private Text leftUSError;
+
+        [SerializeField]
+        private Text rightUSError;
+
+        [SerializeField]
+        private Text backUSError;
+
+        [SerializeField]
+        private Text leftIRError;
+
+        [SerializeField]
+        private Text middleIRError;
+
+        [SerializeField]
+        private Text rightIRError;
+
+        [SerializeField]
+        private Text leftUSValue;
+
+        [SerializeField]
+        private Text rightUSValue;
+
+        [SerializeField]
+        private Text backUSValue;
+
+        [SerializeField]
+        private Text leftIRValue;
+
+        [SerializeField]
+        private Text middleIRValue;
+
+        [SerializeField]
+        private Text rightIRValue;
+
         private USSensor mLeftUSSensor;
         private USSensor mRightUSSensor;
         private USSensor mBackUSSensor;
@@ -28,7 +65,19 @@ namespace BuddyApp.Diagnostic
 
         void Update()
         {
+            leftUSError.text = "Left US err : " + mLeftUSSensor.Error;
+            rightUSError.text = "Right US err : " + mRightUSSensor.Error;
+            backUSError.text = "Back US err : " + mBackUSSensor.Error;
+            leftUSValue.text = "Left US val : " + mLeftUSSensor.Value;
+            rightUSValue.text = "Right US val : " + mRightUSSensor.Value;
+            backUSValue.text = "Back US val : " + mBackUSSensor.Value;
 
+            leftIRError.text = "Left IR err : " + mLeftIRSensor.Error;
+            middleIRError.text = "Middle IR err : " + mMiddleIRSensor.Error;
+            rightIRError.text = "Right IR err : " + mRightIRSensor.Error;
+            leftIRValue.text = "Left IR val : " + mLeftIRSensor.Value;
+            middleIRValue.text = "Middle IR val : " + mMiddleIRSensor.Value;
+            rightIRValue.text = "Right IR val : " + mRightIRSensor.Value;
         }
     }
 }
