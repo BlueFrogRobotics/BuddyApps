faa243ede6fa4919d16b7b4120f5628f382cabba
Karama <kg@bluefrogrobotics.com>
2016-12-06 17:03:41 +0100

    [BABYPHONE]: head control state

--- Assets/BabyPhone/Scripts/Data/BabyPhoneData.cs
+++ Assets/BabyPhone/Scripts/Data/BabyPhoneData.cs
@@ -7,6 +7,16 @@ namespace BuddyApp.BabyPhone
         public int Timer { get; set; }
         public bool TimerIsActive { get; set; }
         public string SongToPlay { get; set; }
+        //sauvegarde des paramètres YES/NO ? 
+        //choix du contact
+        //berceuse ON/OFF
+        //choix de la berceuse 
+        //le volume pour la berceuse 
+        //visage ON/OFF
+        //choix des "visages" à jouer
+        //le niveau de la luminosité de l'écran pour le jeu dus visages
+        //le temps "d'endormissement", le temps de jouer la berceuse
+        //le temps avant contact = combien de temps, si on détecte les pleurs, avant d'en informer le contact?
 
         public static BabyPhoneData Instance
         {
