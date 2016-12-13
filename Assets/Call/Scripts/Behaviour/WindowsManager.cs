21928b70d27910f1b36153c91560db3c23385e10
marcHulcelle <mh@bluefrogrobotics.com>
2016-12-13 18:52:24 +0100

    [REMOTE] Deleted old scene RemoteControl to switch to Call scene. Also added dictionary for companion App

---
+++ Assets/Call/Scripts/Behaviour/WindowsManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using BuddyOS;
+using System.Collections;
+
+namespace BuddyApp.Call
+{
+    public class WindowsManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator receiveCallAnim;
+
+        [SerializeField]
+        private Animator receiveCallTimeAnim;
+
+        [SerializeField]
+        private Animator backgroundAnim;
+
+        [SerializeField]
+        private Animator callAnimator;
+
+        private bool mIncomingCallHandled;
+
+        void Start()
+        {
+            receiveCallAnim.SetTrigger("Close_WReceiveCall");
+            receiveCallTimeAnim.SetTrigger("Close_WReceiveCallTime");
+            callAnimator.SetTrigger("Close_WCall");
+            backgroundAnim.SetTrigger("Open_BG");
+            mIncomingCallHandled = false;
+        }
+        
+        void Update()
+        {
+
+        }
+
+        public void GetIncomingCall()
+        {
+            if (mIncomingCallHandled)
+                return;
+
+            mIncomingCallHandled = true;
+            receiveCallAnim.SetTrigger("Open_WReceiveCall");
+        }
+
+        public void StopCall()
+        {
+            if (!mIncomingCallHandled)
+                return;
+
+            mIncomingCallHandled = false;
+            callAnimator.SetTrigger("Close_WCall");
+        }
+    }
+}
