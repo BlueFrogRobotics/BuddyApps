d2e10040c8e9e0b9e042e6e3cd1616819ff0b432
Gregoire Milliez <gm@bluefrogrobotics.com>
2016-12-23 12:51:40 +0100

    [MEMORY] first functional version of memory game

---
+++ Assets/Memory/Script/Behaviour/LinkStateMachineBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace BuddyApp.Memory
+{
+	public class LinkStateMachineBehavior : StateMachineBehaviour
+	{
+		public LinkHandler link;
+
+		// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+		override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+		}
+		
+	}
+}
