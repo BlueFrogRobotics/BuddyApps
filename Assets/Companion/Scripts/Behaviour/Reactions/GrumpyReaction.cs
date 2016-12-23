51fb4f858e602f7ed301aba094756119e8956aa1
marcHulcelle <mh@bluefrogrobotics.com>
2016-12-23 18:05:17 +0100

    [COMPANION] Added vocal commands to launch application, set head position. Also new reaction when grumpy

---
+++ Assets/Companion/Scripts/Behaviour/Reactions/GrumpyReaction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace BuddyApp.Companion
+{
+    public class GrumpyReaction : MonoBehaviour
+    {
+        public float AnimationTime { get { return mAnimTime; } }
+
+        [SerializeField]
+        private Emotion mEmotion;
+
+        private float mAnimTime;
+
+        private float mAnimStartTime;
+
+        void OnEnable()
+        {
+            mEmotion.EnableChoregraph();
+            mAnimStartTime = Time.time;
+            switch(Random.Range(0, 2))
+            {
+                case 0:
+                    mAnimTime = 6.0F;
+                    mEmotion.SetEmotion(EmotionType.GRUMPY);
+                    mEmotion.SetEvent(EmotionEvent.GRUMPY);
+                    break;
+                case 1:
+                    mAnimTime = 4.5F;
+                    mEmotion.SetEmotion(EmotionType.ANGRY);
+                    mEmotion.SetEvent(EmotionEvent.ANGRY);
+                    break;
+            }
+        }
+        
+        void Update()
+        {
+            if(Time.time - mAnimStartTime > mAnimTime) {
+                mEmotion.SetEmotion(EmotionType.NEUTRAL);
+                enabled = false;
+            }
+        }
+
+        void OnDisable()
+        {
+            mEmotion.DisableChoregraph();
+            GetComponent<Reaction>().ActionFinished();
+        }
+    }
+}
