30df91904beea97d69428b1da98431c8f6b99219
Ulysse VAUTIER <uv@bluefrogrobotics.com>
2016-12-30 18:29:50 +0100

    [IOT]Merge?

---
+++ Assets/IOT/Scripts/Commands/IOTDeviceCmdCmd.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using BuddyOS.Command;
+
+namespace BuddyApp.IOT
+{
+    public class IOTDeviceCmdCmd : ACommand
+    {
+        public IOTDeviceCmdCmd(object iObject, int iInt)
+        {
+            Parameters = new CommandParam();
+            Parameters.Objects = new object[1] { iObject };
+            Parameters.Integers = new int[1] { iInt };
+        }
+        protected override void ExecuteImpl()
+        {
+            ((IOTDevices)Parameters.Objects[0]).Command(Parameters.Integers[0]);
+        }
+    }
+}
