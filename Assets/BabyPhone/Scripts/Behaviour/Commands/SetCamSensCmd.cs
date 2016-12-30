6adc1de523407b45ab43ac950df06b96df2384a5
Karama <kg@bluefrogrobotics.com>
2016-12-30 14:51:29 +0100

    [BABYPHONE] : add animation, and warnings

---
+++ Assets/BabyPhone/Scripts/Behaviour/Commands/SetCamSensCmd.cs
@@ -0,0 +1,12 @@
+﻿using BuddyOS.Command;
+
+namespace BuddyApp.BabyPhone
+{
+    internal class SetCamSensCmd : ACommand
+    {
+        protected override void ExecuteImpl()
+        {
+            BabyPhoneData.Instance.CameraSensitivity = Parameters.Integers[0];
+        }
+    }
+}
