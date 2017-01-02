be161c3be9364b4b9131e088e8e43513e5554ad8
Karama <kg@bluefrogrobotics.com>
2017-01-02 11:16:36 +0100

    [BABYPHONE]: add motion and sound detection debug

---
+++ Assets/BabyPhone/Scripts/Behaviour/DebugSoundDetection.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using BuddyOS.UI;
+using BuddyOS;
+using System.Collections.Generic;
+
+namespace BuddyApp.BabyPhone
+{
+    public class DebugSoundDetection : MonoBehaviour
+    {
+        private Animator debugSoundAnimator;
+
+        [SerializeField]
+        private Gauge microSensitivity;
+
+
+        [SerializeField]
+        private Text labelSound;
+
+
+        private Dictionary mDictionary;
+
+        void OnEnable()
+        {
+            mDictionary = BYOS.Instance.Dictionary;
+            debugSoundAnimator = GetComponent<Animator>();
+            debugSoundAnimator.SetTrigger("Open_WDebugs");
+        }
+
+        void OnDisable()
+        {
+            debugSoundAnimator.SetTrigger("Close_WDebugs");
+        }
+
+        void Start()
+        {
+            Init();
+            Labelize();
+        }
+
+        public void Labelize()
+        {
+            microSensitivity.Label.text = mDictionary.GetString("soundetect");
+            labelSound.text = mDictionary.GetString("microsens");
+        }
+
+        public void Init()
+        {
+            ////sound detection
+            microSensitivity.DisplayPercentage = true;
+            microSensitivity.Slider.minValue = 5;
+            microSensitivity.Slider.maxValue = 20;
+            microSensitivity.Slider.value = BabyPhoneData.Instance.MicrophoneSensitivity;
+            microSensitivity.UpdateCommands.Add(new SetMicroSensCmd());
+        }
+
+
+    }
+}
