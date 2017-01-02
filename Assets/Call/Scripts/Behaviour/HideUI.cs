afed709d4958e484708da8439070daee50257be0
marcHulcelle <mh@bluefrogrobotics.com>
2017-01-02 19:11:21 +0100

    [COMPANION][CALL] Updated SearchFace reaction for quicker finding. Implemented button interactions for call

---
+++ Assets/Call/Scripts/Behaviour/HideUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BuddyApp.Call
+{
+    public class HideUI : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject feedBack;
+        [SerializeField]
+        private GameObject callerVideo;
+        [SerializeField]
+        private GameObject buddyFace;
+
+        private bool mUIEnabled;
+
+        void Start()
+        {
+            mUIEnabled = true;
+        }
+        
+        void Update()
+        {
+
+        }
+
+        public void SwitchUI()
+        {
+            if(mUIEnabled) {
+                callerVideo.SetActive(false);
+                feedBack.SetActive(false);
+                buddyFace.SetActive(false);
+                mUIEnabled = false;
+            } else {
+                callerVideo.SetActive(true);
+                feedBack.SetActive(true);
+                //buddyFace.SetActive(false);
+                mUIEnabled = true;
+            }
+        }
+    }
+}
