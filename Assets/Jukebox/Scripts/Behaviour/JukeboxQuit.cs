5a2a2407d5bcef1210981d5327a52e5af96fc907
Mickael <mc@bluefrogrobotics.com>
2017-01-05 11:14:33 +0100

    [APP][jukebox] fix problem UI, volume, button quit, addition button roombawalk

---
+++ Assets/Jukebox/Scripts/Behaviour/JukeboxQuit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using BuddyOS;
+using BuddyOS.Command;
+
+namespace BuddyApp.Jukebox
+{
+    public class JukeboxQuit : MonoBehaviour
+    {
+
+        private TextToSpeech mTTS;
+        void Start()
+        {
+            mTTS = BYOS.Instance.TextToSpeech;
+        }
+        public void QuitApplication()
+        {
+            Debug.Log("Quit app");
+            mTTS.Silence(0);
+            new HomeCmd().Execute();
+        }
+    }
+}
