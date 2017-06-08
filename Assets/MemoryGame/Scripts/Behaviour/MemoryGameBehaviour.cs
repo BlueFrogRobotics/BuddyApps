1629470a3ecb61b46cbf9c073afe549955471345
GregoireMilliez <gm@bluefrogrobotics.com>
2017-06-08 19:26:07 +0200

    [MEMORYGAME] adding memory game, now using the face events for click

---
+++ Assets/MemoryGame/Scripts/Behaviour/MemoryGameBehaviour.cs
@@ -0,0 +1,37 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+using Buddy;
+
+namespace BuddyApp.MemoryGame
+{
+    /* A basic monobehaviour as "AI" behaviour for your app */
+    public class MemoryGameBehaviour : MonoBehaviour
+    {
+        /*
+         * API of the robot
+         */
+        private TextToSpeech mTextToSpeech;
+
+        /*
+         * Data of the application. Save on disc when app quit happened
+         */
+        private MemoryGameData mAppData;
+
+        /*
+         * Init refs to API and your app data
+         */
+        void Start()
+        {
+            mTextToSpeech = BYOS.Instance.TextToSpeech;
+            mAppData = MemoryGameData.Instance;
+        }
+
+        /*
+         * A sample of use of data (here for basic display purpose)
+         */
+        void Update()
+        {
+        }
+    }
+}
---
+++ Assets/MemoryGame/Scripts/Behaviour/WaitFaceReady.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace BuddyApp.MemoryGame
+{
+	public class WaitFaceReady : AStateMachineBehaviour
+	{
+
+		float timer = 0.0f;
+
+		public override void Start()
+		{
+		}
+
+		// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+		public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+
+		}
+
+
+		// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+		public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			timer += Time.deltaTime;
+
+			//if (link.mFace.IsStable && timer > 1.0f) {
+			if (timer > 1.0f) {
+				animator.SetTrigger("FaceReady");
+			}
+		}
+
+
+		public override void OnStateExit(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
+		{
+		}
+
+	}
+}
