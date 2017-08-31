f97d0e6f8e8c031af3dc64f33a04cf97f6b10acc
GregoireMilliez <gm@bluefrogrobotics.com>
2017-08-31 18:26:47 +0200

    [WEATHER] fixing weather time

--- Assets/Weather/Scripts/Behaviour/APIRequest.cs
+++ Assets/Weather/Scripts/Behaviour/APIRequest.cs
@@ -9,14 +9,15 @@ namespace BuddyApp.Weather
 {
 	public class APIRequest : AStateMachineBehaviour
 	{
-        private int mDate;
-        //Max numberWeatherInfos égal à 64
-        private int mNumberWeatherInfos = 60;
+		private int mDate;
+		//Max numberWeatherInfos égal à 64
+		private int mNumberWeatherInfos = 60;
 		private bool mAnswerReceived;
 		private bool mQuit;
 
 		// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-		override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
 
 			Debug.Log("ENTER API REQUEST");
 			mAnswerReceived = false;
@@ -34,12 +35,12 @@ namespace BuddyApp.Weather
 			Debug.Log("Post web service ");
 
 
-        }
+		}
 
 
 
 		//OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 		{
 			if (mAnswerReceived) {
 				Trigger("Restitution");
@@ -52,27 +53,37 @@ namespace BuddyApp.Weather
 
 
 
-		private void WeatherProcessing(WeatherInfo[] iWeather)
+		private void WeatherProcessing(WeatherInfo[] iWeather, WEATHER_ERROR iError)
 		{
 			Debug.Log("WeatherProcessing");
-			if (iWeather == null || iWeather.Length == 0)
-            {
+			if (iError != WEATHER_ERROR.NONE) {
+				if (iError == WEATHER_ERROR.UNKNOWN_LOCATION)
+					Interaction.TextToSpeech.SayKey("locationissue");
+				else if (iError == WEATHER_ERROR.GEOLOCALIZATION_FAILED)
+					Interaction.TextToSpeech.SayKey("geolocfailed");
+				else if (iError == WEATHER_ERROR.GEOLOCALIZATION_DISABLED)
+					Interaction.TextToSpeech.SayKey("geolocdisable");
+
+
+
+
+				mQuit = true;
+				return;
+			}
+			// If error are handle correctly, this shouldn't happen!
+			else if (iWeather == null || iWeather.Length == 0) {
 				//Unknown location
 				Interaction.TextToSpeech.SayKey("locationissue");
 				mQuit = true;
-                return;
-                //Say la location n'est pas reconnue / n'existe pas
-            }
-            else
-            {
-                Debug.Log("LOCATION : " + iWeather[0].Location);
-                if ( WeatherData.Instance.Date < 7)
-                {
-                    //pas grave si ça dépasse 64 parce que ça ne sera un écart que de quelques heures pour la météo dans 6 jours pour le max
-                    mDate = 8 * WeatherData.Instance.Date + 4;
-                    
-                }
-                else if (WeatherData.Instance.Date > 7) {
+				return;
+				//Say la location n'est pas reconnue / n'existe pas
+			} else {
+				Debug.Log("LOCATION : " + iWeather[0].Location);
+				if (WeatherData.Instance.Date < 7) {
+					//pas grave si ça dépasse 64 parce que ça ne sera un écart que de quelques heures pour la météo dans 6 jours pour le max
+					mDate = 8 * WeatherData.Instance.Date + 2;
+
+				} else if (WeatherData.Instance.Date > 7) {
 					Interaction.TextToSpeech.SayKey("oneweekmax");
 					mQuit = true;
 					return;
@@ -82,5 +93,5 @@ namespace BuddyApp.Weather
 			}
 			mAnswerReceived = true;
 		}
-    }
+	}
 }
