57e81e1d8ae1191c93d12061e719a73cffafb4e5
mickael <mc@bluefrogrobotics.com>
2017-10-05 10:28:43 +0200

    [RLGL] first commit of the new version

---
+++ Assets/RedLightGreenLight/Scripts/Behaviour/Features/StateMachine/RedLightGreenLightStateMachineManager.cs
@@ -0,0 +1,81 @@
+using Buddy;
+
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Your script app namespace. You must keep BuddyApp.YOUR_APP namespace for every script file
+/// </summary>
+namespace BuddyApp.RedLightGreenLight
+{
+    /// <summary>
+    /// If you planned to use a state machine for your application, and want to use provided AStateMachineBehaviour,
+    /// you will need to put the StateMachineAppLinker as component of a gameobject in your scene.
+    /// You can have as many Linker as you have state machine.
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    public sealed class RedLightGreenLightStateMachineManager : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> gameObjects = new List<GameObject>();
+
+        private Animator mAnimator;
+
+        private Dictionary<string, int> mCommonIntegers;
+        private Dictionary<string, float> mCommonSingles;
+        private Dictionary<string, string> mCommonStrings;
+        private Dictionary<string, object> mCommonObjects;
+
+        internal List<GameObject> GameObjects { get { return gameObjects; } }
+
+        void Start()
+        {
+            mAnimator = GetComponent<Animator>();
+            if (mAnimator != null) {
+                mAnimator.enabled = true;
+
+                AStateMachineBehaviour[] lStates = mAnimator.GetBehaviours<AStateMachineBehaviour>();
+                mCommonIntegers = new Dictionary<string, int>();
+                mCommonSingles = new Dictionary<string, float>();
+                mCommonStrings = new Dictionary<string, string>();
+                mCommonObjects = new Dictionary<string, object>();
+
+                foreach (AStateMachineBehaviour lState in lStates) {
+                    lState.Init();
+                    lState.CommonIntegers = mCommonIntegers;
+                    lState.CommonSingles = mCommonSingles;
+                    lState.CommonStrings = mCommonStrings;
+                    lState.CommonObjects = mCommonObjects;
+                    lState.Animator = mAnimator;
+                    lState.Manager = this;
+                    lState.Start();
+                }
+            }
+            else
+                Utils.LogE(LogContext.APP, LogInfo.NULL_VALUE,
+                    "Animator of the state machine manager is not set", true);
+        }
+
+        internal void AddComponentLink<T>() where T : Component
+        {
+            gameObject.AddComponent<T>();
+        }
+
+        internal void StopAllCoroutinesLink()
+        {
+            StopAllCoroutines();
+        }
+
+        internal void StopCoroutineLink(IEnumerator iFunc)
+        {
+            StopCoroutine(iFunc);
+        }
+
+        internal void StartCoroutineLink(IEnumerator iFunc)
+        {
+            StartCoroutine(iFunc);
+        }
+    }
+}
