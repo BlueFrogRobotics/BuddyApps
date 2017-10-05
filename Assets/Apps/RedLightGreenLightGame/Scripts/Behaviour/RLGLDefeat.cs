654a4bf05a63cc0eb7ae8baed75e06d71ab887d1
GregoireMilliez <gm@bluefrogrobotics.com>
2017-10-05 17:41:42 +0200

    [RLGL] Adding victory + defeat

--- Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/RLGLDefeat.cs
+++ Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/RLGLDefeat.cs
@@ -11,8 +11,11 @@ namespace BuddyApp.RedLightGreenLightGame
         // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-
-        }
+			Interaction.Mood.Set(MoodType.SAD);
+			Interaction.TextToSpeech.SayKey("defeat");
+			Interaction.TextToSpeech.Silence(1000);
+			Interaction.TextToSpeech.Say(Dictionary.GetRandomString("loselevel"), true);
+		}
 
         // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -23,8 +26,8 @@ namespace BuddyApp.RedLightGreenLightGame
         // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
         override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-
-        }
+			Interaction.Mood.Set(MoodType.NEUTRAL);
+		}
     }
 
 }
--- Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/RLGLVictory.cs
+++ Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/RLGLVictory.cs
@@ -1,29 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Buddy;
 
 namespace BuddyApp.RedLightGreenLightGame
 {
-    public class RLGLVictory : StateMachineBehaviour
-    {
+    public class RLGLVictory : AStateMachineBehaviour
+	{
 
         // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-
+			//TODO take real level
+			int lLvl = 1;
+			Interaction.Mood.Set(MoodType.HAPPY);
+			Interaction.Face.SetEvent(FaceEvent.SMILE);
+			Interaction.TextToSpeech.SayKey("victory");
+			Interaction.TextToSpeech.Silence(1000);
+			Interaction.TextToSpeech.Say(Dictionary.GetRandomString("wonlevel") + lLvl, true);
         }
 
         // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-
+			Trigger("Repositionning");
         }
 
         // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
         override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
 
-        }
+			Interaction.Mood.Set(MoodType.NEUTRAL);
+		}
 
     }
 
