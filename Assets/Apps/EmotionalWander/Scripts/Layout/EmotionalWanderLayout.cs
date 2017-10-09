ec933b0c543c9255fb4d2aac3616d6e12f1f300d
GregoireMilliez <gm@bluefrogrobotics.com>
2017-10-09 18:19:13 +0200

    [EMOTIONALWANDER] proto of emotional wander

---
+++ Assets/Apps/EmotionalWander/Scripts/Layout/EmotionalWanderLayout.cs
@@ -0,0 +1,46 @@
+using Buddy.UI;
+
+namespace BuddyApp.EmotionalWander
+{
+    public class EmotionalWanderLayout : AWindowLayout
+    {
+		private Gauge mGaugeValOne;
+		
+        public override void Build()
+        {		
+            /*
+             * Create needed widgets
+             * ==> Which widget do I need for my app settings ?
+             */
+            mGaugeValOne = CreateWidget<Gauge>();
+
+            /*
+             * Set widgets parameters
+             */
+            mGaugeValOne.Slider.minValue = 0;
+            mGaugeValOne.Slider.maxValue = 10;
+            mGaugeValOne.Slider.wholeNumbers = true;
+            mGaugeValOne.DisplayPercentage = true; /* Only the display will be in percentage, the value will still be within 0 and 10 */
+
+            /*
+             * Retrieve app data and display them inside the view
+             * ==> What info must be displayed ?
+             */ 
+            mGaugeValOne.Slider.value = EmotionalWanderData.Instance.MyValue;
+
+            /*
+            * Set command to widgets
+            * At each interaction with a widget, a callback will be called
+            * ==> What must happen when I interacted with a widget ?
+            */
+            mGaugeValOne.OnUpdateEvent((iVal) => {
+				EmotionalWanderData.Instance.MyValue = iVal;
+			});
+		}
+
+        public override void LabelizeWidgets()
+        {
+            mGaugeValOne.Label = "AN INTEGER";
+        }
+    }
+}
