655583aa0f30711f9b218e8be433435a99dbeefc
macpc <macpc@DESKTOP-J83BM5L>
2017-11-17 11:17:22 +0100

    Add Timer

---
+++ Assets/Apps/Timer/Scripts/Behaviour/BipSounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Buddy;
+using Buddy.UI;
+
+namespace BuddyApp.Timer
+{
+    public class BipSounds : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
