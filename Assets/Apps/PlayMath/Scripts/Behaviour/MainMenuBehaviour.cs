9ceb04ccfa160d423487c637036b91f9345a9303
florianc <florian.cantori@awabot.com>
2017-11-20 17:37:14 +0100

    [PLAYMATH] Implement app dictionnary for MainMenu and Settings

--- Assets/Apps/PlayMath/Scripts/Behaviour/MainMenuBehaviour.cs
+++ Assets/Apps/PlayMath/Scripts/Behaviour/MainMenuBehaviour.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using Buddy;
 
 namespace BuddyApp.PlayMath{
 	public class MainMenuBehaviour : MonoBehaviour
 	{
-
+        [SerializeField]
 		private Animator mPlayMathAnimator;
 
+        private Text mTitleTop;
+        private Text mTablesButton;
+        private Text mSettingsButton;
+        private Text mPlayButton;
+        private Text mScoresButton;
+        private Text mCertifButton;
+
 		public void Start() {
-			mPlayMathAnimator = GameObject.Find("AIBehaviour").GetComponent<Animator>();
+            // Retrieve every child text component
+            mTitleTop = this.gameObject.transform.Find("Top_UI/Title_Top").GetComponent<Text>();
+            mTablesButton = this.gameObject.transform.Find("Middle_UI/Button_Tables/Text").GetComponent<Text>();
+            mSettingsButton = this.gameObject.transform.Find("Middle_UI/Button_Settings/Text").GetComponent<Text>();
+            mPlayButton = this.gameObject.transform.Find("Middle_UI/Button_Play/Text").GetComponent<Text>();
+            mScoresButton = this.gameObject.transform.Find("Middle_UI/Button_Scores/Text").GetComponent<Text>();
+            mCertifButton = this.gameObject.transform.Find("Middle_UI/Button_Certificate/Text").GetComponent<Text>();
+
+            TranslateUI();
 		}
+
+        private void TranslateUI(){
+            mTitleTop.text = BYOS.Instance.Dictionary.GetString("menutitle").ToUpper();
+            mTablesButton.text = BYOS.Instance.Dictionary.GetString("tablelabel").ToUpper();
+            mSettingsButton.text = BYOS.Instance.Dictionary.GetString("settingslabel").ToUpper();
+            mPlayButton.text = BYOS.Instance.Dictionary.GetString("playlabel").ToUpper();
+            mScoresButton.text = BYOS.Instance.Dictionary.GetString("leaderboardlabel").ToUpper();
+            mCertifButton.text = BYOS.Instance.Dictionary.GetString("certificationlabel").ToUpper();
+        }
 
 		public void OnClickSettings() {
 			mPlayMathAnimator.SetTrigger("GameSettings");
--- Assets/Apps/PlayMath/Scripts/Behaviour/SettingsBehaviour.cs
+++ Assets/Apps/PlayMath/Scripts/Behaviour/SettingsBehaviour.cs
@@ -9,7 +9,7 @@ using Buddy;
 namespace BuddyApp.PlayMath{
 	public class SettingsBehaviour : MonoBehaviour
 	{
-
+        [SerializeField]
 		private Animator mPlayMathAnimator;
 
 		private GameParameters mGameParameters;
@@ -31,14 +31,47 @@ namespace BuddyApp.PlayMath{
 		[SerializeField]
 		private Toggle mToggleDiv;
 
+        private Text mTitleTop;
+        private Text mDifficultyLabel;
+        private Text mGoToMenu;
+        private Text mResetScores;
+        private Text mPlay;
+
 		public void Start() {
 			mGameParameters = User.Instance.GameParameters;
 
-			mPlayMathAnimator = GameObject.Find("AIBehaviour").GetComponent<Animator>();
+            mTitleTop = this.gameObject.transform.Find("Top_UI/Title_Top").GetComponent<Text>();
+            mDifficultyLabel = this.gameObject.transform.Find("Middle_UI/Content/Text_level").GetComponent<Text>();
+            mGoToMenu = this.gameObject.transform.Find("Bottom_UI/Button_Menu/Text").GetComponent<Text>();
+            mResetScores = this.gameObject.transform.Find("Bottom_UI/Button_Reset/Text").GetComponent<Text>();
+            mPlay = this.gameObject.transform.Find("Bottom_UI/Button_Play/Text").GetComponent<Text>();
+
+            TranslateUI();
 
 			BindOperands();
 			BindStars();
 		}
+
+        private void TranslateUI()
+        {
+            mTitleTop.text = BYOS.Instance.Dictionary.GetString("settingstitle").ToUpper();
+            mDifficultyLabel.text = BYOS.Instance.Dictionary.GetString("difficultylabel").ToUpper();
+            mGoToMenu.text = BYOS.Instance.Dictionary.GetString("gotomenulabel").ToUpper();
+            mResetScores.text = BYOS.Instance.Dictionary.GetString("resetscoreslabel").ToUpper();
+            mPlay.text = BYOS.Instance.Dictionary.GetString("playlabel").ToUpper();
+
+            Text lText = mToggleAdd.gameObject.transform.Find("Label").GetComponent<Text>();
+            lText.text = BYOS.Instance.Dictionary.GetString("additionlabel").ToUpper();
+
+            lText = mToggleSub.gameObject.transform.Find("Label").GetComponent<Text>();
+            lText.text = BYOS.Instance.Dictionary.GetString("subtractionlabel").ToUpper();
+
+            lText = mToggleMulti.gameObject.transform.Find("Label").GetComponent<Text>();
+            lText.text = BYOS.Instance.Dictionary.GetString("multiplylabel").ToUpper();
+
+            lText = mToggleDiv.gameObject.transform.Find("Label").GetComponent<Text>();
+            lText.text = BYOS.Instance.Dictionary.GetString("divisionlabel").ToUpper();
+        }
 
 		public void OnClickGoToMenu() {
 			if (! CheckSettings()) {
