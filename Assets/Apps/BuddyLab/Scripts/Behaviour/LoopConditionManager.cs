31e43e6cfe5daf64b26a91183beaef411066785c
walid <wa@bluefrogrobotics.com>
2018-01-24 16:47:09 +0100

    [APP][BUDDYLAB] added condition choice window in sensor and vision loops

---
+++ Assets/Apps/BuddyLab/Scripts/Behaviour/LoopConditionManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BuddyApp.BuddyLab
+{
+    public class LoopConditionManager : MonoBehaviour
+    {
+        [SerializeField]
+        private ConditionChoiceWindow conditionChoiceWindow;
+
+        [SerializeField]
+        private Image icon;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ShowWindow()
+        {
+            Debug.Log("le show button");
+            DragAndDropCell lCell = GetComponentInParent<DragAndDropCell>();
+            if (lCell != null && lCell.cellType == DragAndDropCell.CellType.Swap)
+            {
+                conditionChoiceWindow.OnChangeCondition += OnConditionChoice;
+                conditionChoiceWindow.ShowWindow();
+            }
+        }
+
+        private void OnConditionChoice(string iParam, Sprite iSprite)
+        {
+            icon.sprite = iSprite;
+            GetComponent<ABLItem>().Parameter = iParam;
+            conditionChoiceWindow.OnChangeCondition -= OnConditionChoice;
+        }
+    }
+}
