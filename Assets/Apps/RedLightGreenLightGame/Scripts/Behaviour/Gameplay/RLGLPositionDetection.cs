948be9e803fb146a8ee6c4316825c0effcacca56
mc <mc@bluefrogrobotics.com>
2018-03-07 10:16:24 +0100

    [BuddyLab] Add feature for Qr code detection and untested feature about color detection

--- Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/Gameplay/RLGLPositionDetection.cs
+++ Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/Gameplay/RLGLPositionDetection.cs
@@ -26,7 +26,6 @@ namespace BuddyApp.RedLightGreenLightGame
         // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            Debug.Log("POSITIONDETECTION LOL LOL LOL LOL LOL");
             //Interaction.TextToSpeech.Silence(1000);
             mMotion = Perception.Motion;
             mMotion.enabled = true;
@@ -34,7 +33,7 @@ namespace BuddyApp.RedLightGreenLightGame
             mMotion.OnDetect(OnMovementDetected, 3f);
 
             mMat = mCam.FrameMat.clone();
-			Mat mMatSrc = mCam.FrameMat;
+			Mat mMatSrc = mCam.FrameMat.clone();
 			Core.flip(mMatSrc, mMat, 1);
 			mTexture = Utils.MatToTexture2D(mMat);
             mMatDetection = null;
