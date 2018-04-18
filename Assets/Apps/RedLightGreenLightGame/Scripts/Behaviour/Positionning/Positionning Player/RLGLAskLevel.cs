1c795c0f5810eb22441b490846e357b4a40acd4c
walid <wa@bluefrogrobotics.com>
2018-04-18 11:24:36 +0200

    [APP][RLGL] stop asking the level if the user don't tell the level after 10s

--- Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/Positionning/Positionning Player/RLGLAskLevel.cs
+++ Assets/Apps/RedLightGreenLightGame/Scripts/Behaviour/Positionning/Positionning Player/RLGLAskLevel.cs
@@ -24,6 +24,7 @@ namespace BuddyApp.RedLightGreenLightGame
             mRLGLBehaviour.Timer = 0;
             mSwitchState = false;
             //Interaction.Mood.Set(MoodType.THINKING);
+            Interaction.SpeechToText.OnErrorEnum.Add(ErrorSTT);
             Interaction.SpeechToText.OnBestRecognition.Add(OnRecognition);
             StartCoroutine(AskLevel());
         }
@@ -31,11 +32,14 @@ namespace BuddyApp.RedLightGreenLightGame
         // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            if (mRLGLBehaviour.Timer > 6.0f)
+            if (mRLGLBehaviour.Timer > 10.0f && !mSwitchState && !mListening)
             {
                 Interaction.Mood.Set(MoodType.NEUTRAL);
-                mListening = false;
+                //mListening = false;
                 mRLGLBehaviour.Timer = 0.0f;
+                mSwitchState = true;
+                GetComponentInGameObject<LevelManager>(0).SetLevel(0);
+                Trigger("LevelAsked");
             }
 
             if (!Interaction.TextToSpeech.HasFinishedTalking || mListening)
@@ -63,6 +67,7 @@ namespace BuddyApp.RedLightGreenLightGame
         {
             Interaction.Mood.Set(MoodType.NEUTRAL);
             Interaction.SpeechToText.OnBestRecognition.Remove(OnRecognition);
+            Interaction.SpeechToText.OnErrorEnum.Remove(ErrorSTT);
         }
 
         private IEnumerator AskLevel()
@@ -70,9 +75,15 @@ namespace BuddyApp.RedLightGreenLightGame
             string lText = Dictionary.GetRandomString("asklevel");
             lText = lText.Replace("[maxlevel]", "" + 10);
             mTTS.Say(lText);
+            mRLGLBehaviour.Timer = 0;
             yield return null;
             //yield return SayKeyAndWait("asklevel");
 
+        }
+
+        void ErrorSTT(STTError iSpeech)
+        {
+            mListening = false;
         }
 
         private void OnRecognition(string iText)
