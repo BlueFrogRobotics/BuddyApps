e6ebd69404e0c89552536a4f130211126247469f
walid <wa@bluefrogrobotics.com>
2018-05-03 15:41:38 +0200

    [APP][REMOTECONTROL] added in remotecontrolbehiour script a method to send a notification to the mobile app using firebase

--- Assets/Apps/RemoteControl/Scripts/Behaviour/RemoteControlBehaviour.cs
+++ Assets/Apps/RemoteControl/Scripts/Behaviour/RemoteControlBehaviour.cs
@@ -1,5 +1,6 @@
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.Networking;
 
 using Buddy;
 using System;
@@ -162,6 +163,51 @@ namespace BuddyApp.RemoteControl
                 
             }
             yield return null;
+        }
+
+        /// <summary>
+        /// Will send a notification to the mobile app
+        /// </summary>
+        public void SendMessageFirebase()
+        {
+            Debug.Log("Send message");
+            JSONNode lNode = new JSONObject();
+
+            ///It's the key of the firebase server
+            string lServerKey = "dZ416EcYA0s:APA91bFeYlrC6h5ykx6HN7cvYaDllWvaB_ZF5Iu7eHnZ48Vv4008x0293SQEnPbc8Eu54xYPPr3ynhcYce1XcZCFQSrIUJxZefukCTCXxMsmGKgE0-EG4t7f-0k8pePgsNXLMGHL2Fdw";
+            lNode.Add("to", lServerKey);
+            JSONNode lNotification = new JSONObject();
+            lNotification.Add("title", "le titre");
+            lNotification.Add("body", "le contenu");
+            lNode.Add("notification", lNotification);
+
+            Debug.Log("json a envoyer: " + lNode.ToString());
+
+            ///TODO: get the token from a database
+            string lDeviceToken = "AAAA6tk1qr0:APA91bEUIFMXzivaYQnPIzfFevwbeqtLgz_MUpzHNd4l3xIQiD6MTZJrTZBPnD7pEEhYYSjbla03pU41wDpiMcpfTg1klA5OQYEq6JWuXdK6ZeGLo6wRDcYKa03XNy3MeojOdPB1ioDN";
+            StartCoroutine(PostRequest("https://fcm.googleapis.com/fcm/send", lNode.ToString(), lDeviceToken));
+        }
+
+        
+        /// <summary>
+        /// Sends a post request to the firebase server
+        /// </summary>
+        /// <param name="url">url of the google api</param>
+        /// <param name="bodyJsonString">the json text to send</param>
+        /// <param name="iDeviceToken">the device token. This should be saved in a database and associated with an account user</param>
+        /// <returns></returns>
+        IEnumerator PostRequest(string url, string bodyJsonString, string iDeviceToken)
+        {
+            UnityWebRequest request = new UnityWebRequest(url, "POST");
+            byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(bodyJsonString);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Authorization", "key=" + iDeviceToken);
+
+            yield return request.Send();
+
+            Debug.Log("Response: " + request.downloadHandler.text);
         }
     }
 }
