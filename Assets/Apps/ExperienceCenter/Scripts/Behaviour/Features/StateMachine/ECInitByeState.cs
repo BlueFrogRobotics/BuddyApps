c4b9280572ba7d6de1e5eb7fe569a40a97e5ac97
Kilian Lebrun <klebrun@bluefrogrobotics.com>
2018-05-31 16:44:05 +0200

    [EXPERIENCECENTER] Add Vocon

--- Assets/Apps/ExperienceCenter/Scripts/Behaviour/Features/StateMachine/ECInitByeState.cs
+++ Assets/Apps/ExperienceCenter/Scripts/Behaviour/Features/StateMachine/ECInitByeState.cs
@@ -6,66 +6,82 @@ using Buddy;
 
 namespace BuddyApp.ExperienceCenter
 {
-	public class ECInitByeState : StateMachineBehaviour
-	{
-		
-		private AnimatorManager mAnimatorManager;
-		private IdleBehaviour mIdleBehaviour;
-		private QuestionsBehaviour mQuestionsBehaviour;
-		private TextToSpeech mTTS;
+    public class ECInitByeState : StateMachineBehaviour
+    {
 
-		private bool mAddReco;
+        private AnimatorManager mAnimatorManager;
+        private IdleBehaviour mIdleBehaviour;
+        private QuestionsBehaviour mQuestionsBehaviour;
+        private TextToSpeech mTTS;
 
-		override public void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-		{
-			mAnimatorManager = GameObject.Find ("AIBehaviour").GetComponent<AnimatorManager> ();
-			mIdleBehaviour = GameObject.Find ("AIBehaviour").GetComponent<IdleBehaviour> ();
-			mQuestionsBehaviour = GameObject.Find ("AIBehaviour").GetComponent<QuestionsBehaviour> ();
-			mTTS = BYOS.Instance.Interaction.TextToSpeech;
+        private bool mAddReco;
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            mAnimatorManager = GameObject.Find("AIBehaviour").GetComponent<AnimatorManager>();
+            mIdleBehaviour = GameObject.Find("AIBehaviour").GetComponent<IdleBehaviour>();
+            mQuestionsBehaviour = GameObject.Find("AIBehaviour").GetComponent<QuestionsBehaviour>();
+            mTTS = BYOS.Instance.Interaction.TextToSpeech;
             BYOS.Instance.Interaction.VocalManager.EnableDefaultErrorHandling = false;
             //BYOS.Instance.Interaction.SpeechToText.OnErrorEnum.Clear();
             //BYOS.Instance.Interaction.SpeechToText.OnErrorEnum.Add(SpeechToTextError);
-			BYOS.Instance.Interaction.VocalManager.OnError = SpeechToTextError;
-			mAddReco = false;
-		}
+            BYOS.Instance.Interaction.VocalManager.OnError = SpeechToTextError;
+            BYOS.Instance.Interaction.VocalManager.UseVocon = true;
+            mAddReco = false;
+        }
 
-		override public void OnStateUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-		{
-			if (!mAddReco && mIdleBehaviour.behaviourEnd && mQuestionsBehaviour.behaviourEnd) {
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (!mAddReco && mIdleBehaviour.behaviourEnd && mQuestionsBehaviour.behaviourEnd)
+            {
                 //if (ExperienceCenterData.Instance.VoiceTrigger)
                 //    BYOS.Instance.Interaction.SphinxTrigger.LaunchRecognition();
                 //BYOS.Instance.Interaction.VocalManager.EnableTrigger = ExperienceCenterData.Instance.VoiceTrigger;
                 BYOS.Instance.Interaction.VocalManager.EnableTrigger = false;
                 //BYOS.Instance.Interaction.SpeechToText.OnBestRecognition.Clear();
                 //BYOS.Instance.Interaction.SpeechToText.OnBestRecognition.Add(SpeechToTextCallback);
-                BYOS.Instance.Interaction.VocalManager.OnEndReco = SpeechToTextCallback;
-				mAddReco = true;
-			}
-		}
+                BYOS.Instance.Interaction.VocalManager.AddGrammar("experiencecenter", LoadContext.APP);
+                BYOS.Instance.Interaction.VocalManager.OnVoconBest = SpeechToTextCallback;
+                //BYOS.Instance.Interaction.VocalManager.OnEndReco = SpeechToTextCallback;
+                mAddReco = true;
+            }
+        }
 
-		public void SpeechToTextCallback (string iSpeech)
-		{
-			Debug.LogFormat ("[EXCENTER] ByeBye - SpeechToText : {0}", iSpeech);
-			bool lClauseFound = false;
-			string[] lPhonetics = BYOS.Instance.Dictionary.GetPhoneticStrings ("byecome");
-			foreach (string lClause in lPhonetics) {
-				if (iSpeech.Contains (lClause)) {
-					lClauseFound = true;
-					break;
-				}
-			} 
-				
-			if (!lClauseFound)
-				Debug.Log ("[EXCENTER] ByeBye - SpeechToText : Not Found");
-			else {
-				mAnimatorManager.ActivateCmd ((byte)(Command.MoveForward));
-			}
-		}
+        public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+        {
+            BYOS.Instance.Interaction.VocalManager.RemoveGrammar("experiencecenter", LoadContext.APP);
+            BYOS.Instance.Interaction.VocalManager.UseVocon = false;
+        }
 
-		public void SpeechToTextError (STTError iError)
-		{
-			Debug.LogWarningFormat ("[EXCENTER][BYE] ERROR STT: {0}", iError.ToString ());
-			BYOS.Instance.Interaction.Mood.Set (MoodType.NEUTRAL);
-		}
-	}
+        public void SpeechToTextCallback(VoconResult iSpeech)
+        {
+            if (string.IsNullOrEmpty(iSpeech.Utterance))
+            {
+                Debug.LogFormat("[EXCENTER] ByeBye - SpeechToText : {0}", iSpeech);
+                bool lClauseFound = false;
+                string[] lPhonetics = BYOS.Instance.Dictionary.GetPhoneticStrings("byecome");
+                foreach (string lClause in lPhonetics)
+                {
+                    if (iSpeech.Utterance.Contains(lClause))
+                    {
+                        lClauseFound = true;
+                        break;
+                    }
+                }
+
+                if (!lClauseFound)
+                    Debug.Log("[EXCENTER] ByeBye - SpeechToText : Not Found");
+                else
+                {
+                    mAnimatorManager.ActivateCmd((byte)(Command.MoveForward));
+                }
+            }
+        }
+
+        public void SpeechToTextError(STTError iError)
+        {
+            Debug.LogWarningFormat("[EXCENTER][BYE] ERROR STT: {0}", iError.ToString());
+            BYOS.Instance.Interaction.Mood.Set(MoodType.NEUTRAL);
+        }
+    }
 }
--- Assets/Apps/ExperienceCenter/Scripts/Behaviour/Features/StateMachine/ECInitState.cs
+++ Assets/Apps/ExperienceCenter/Scripts/Behaviour/Features/StateMachine/ECInitState.cs
@@ -5,46 +5,47 @@ using Buddy;
 
 namespace BuddyApp.ExperienceCenter
 {
-	public class ECInitState : StateMachineBehaviour
-	{
-		
-		private TcpServer mTcpServer;
+    public class ECInitState : StateMachineBehaviour
+    {
 
-		private static bool FirstRun = true;
+        private TcpServer mTcpServer;
 
-		override public void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-		{
-			if (FirstRun) {
-				ExperienceCenterData.Instance.StatusTcp = "Offline";
-				ExperienceCenterData.Instance.IPAddress = "-";
-				ExperienceCenterData.Instance.StopDistance = 0.4F;
-				ExperienceCenterData.Instance.NoiseTime = 0.2F;
-				ExperienceCenterData.Instance.TableDistance = 0.4F;
-				ExperienceCenterData.Instance.IOTDistance = 1.5F;
-				ExperienceCenterData.Instance.NoHingeAngle = 60F;
-				ExperienceCenterData.Instance.NoHingeSpeed = 120F;
-				ExperienceCenterData.Instance.HeadPoseTolerance = 0.2F;
-				ExperienceCenterData.Instance.WelcomeTimeOut = 2F;
-				ExperienceCenterData.Instance.MoveTimeOut = 5F;
-				ExperienceCenterData.Instance.MaxDistance = 1F;
-				ExperienceCenterData.Instance.MinDistance = 0.7F;
-				ExperienceCenterData.Instance.MaxSpeed = 200F;
-				ExperienceCenterData.Instance.MinSpeed = -200F;
-				ExperienceCenterData.Instance.EnableHeadMovement = true;
-				ExperienceCenterData.Instance.EnableBaseMovement = true;
-				ExperienceCenterData.Instance.VoiceTrigger = true;
-				ExperienceCenterData.Instance.EnableBML = true;
-				ExperienceCenterData.Instance.CollisionDebug = false;
-				ExperienceCenterData.Instance.DanceDuration = 30.0f;
-				FirstRun = false;
-			}
+        private static bool FirstRun = true;
 
-			mTcpServer = GameObject.Find ("AIBehaviour").GetComponent<TcpServer> ();
-			mTcpServer.Init ();
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (FirstRun)
+            {
+                ExperienceCenterData.Instance.StatusTcp = "Offline";
+                ExperienceCenterData.Instance.IPAddress = "-";
+                ExperienceCenterData.Instance.StopDistance = 0.4F;
+                ExperienceCenterData.Instance.NoiseTime = 0.2F;
+                ExperienceCenterData.Instance.TableDistance = 0.4F;
+                ExperienceCenterData.Instance.IOTDistance = 1.5F;
+                ExperienceCenterData.Instance.NoHingeAngle = 60F;
+                ExperienceCenterData.Instance.NoHingeSpeed = 120F;
+                ExperienceCenterData.Instance.HeadPoseTolerance = 0.2F;
+                ExperienceCenterData.Instance.WelcomeTimeOut = 2F;
+                ExperienceCenterData.Instance.MoveTimeOut = 5F;
+                ExperienceCenterData.Instance.MaxDistance = 1F;
+                ExperienceCenterData.Instance.MinDistance = 0.7F;
+                ExperienceCenterData.Instance.MaxSpeed = 200F;
+                ExperienceCenterData.Instance.MinSpeed = -200F;
+                ExperienceCenterData.Instance.EnableHeadMovement = true;
+                ExperienceCenterData.Instance.EnableBaseMovement = true;
+                ExperienceCenterData.Instance.VoiceTrigger = true;
+                ExperienceCenterData.Instance.EnableBML = true;
+                ExperienceCenterData.Instance.CollisionDebug = false;
+                ExperienceCenterData.Instance.DanceDuration = 30.0f;
+                FirstRun = false;
+            }
 
-			BYOS.Instance.Interaction.Mood.Set (MoodType.NEUTRAL);
+            mTcpServer = GameObject.Find("AIBehaviour").GetComponent<TcpServer>();
+            mTcpServer.Init();
 
-			BYOS.Instance.Header.DisplayParametersButton = false;
-		}
-	}
+            BYOS.Instance.Interaction.Mood.Set(MoodType.NEUTRAL);
+
+            BYOS.Instance.Header.DisplayParametersButton = false;
+        }
+    }
 }
