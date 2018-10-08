b67dd3257512149dd823c975e16da83b754d0a29
abarabfr <abara@bluefrogrobotics.com>
2018-10-08 17:08:19 +0200

    [REMINDER] Wip on speech parsing function - update dictionary

--- Assets/Apps/Reminder/Scripts/Behaviour/DateChoiceState.cs
+++ Assets/Apps/Reminder/Scripts/Behaviour/DateChoiceState.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BlueQuark;
@@ -7,9 +8,18 @@ namespace BuddyApp.Reminder
 {
     public sealed class DateChoiceState : AStateMachineBehaviour
     {
-        private const int TRY_NUMBER = 2;
+        private const int JANUARY = 1;
+        private const int DECEMBER = 12;
+        private const int TRY_NUMBER = 20;
+        private const string DATE_FORMAT = "dd/MM/yyyy";
         private int mListen;
         private SpeechInputParameters mGrammar;
+
+        // TMP
+        private const string DC_GREEN = "<color=green>-----";
+        private const string DC_RED = "<color=red>-----";
+        private const string DC_BLUE = "<color=blue>-----";
+        private const string DC_END = "-----</color>";
 
         // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -27,23 +37,33 @@ namespace BuddyApp.Reminder
 
         private void VoconGetResult(SpeechInput iSpeechInput)
         {
-            Debug.Log("SPEECH.ToString: " + iSpeechInput.ToString());
-            Debug.Log("SPEECH.Utterance: " + iSpeechInput.Utterance);
+            Debug.Log(DC_BLUE + "SPEECH.ToString: " + iSpeechInput.ToString() + DC_END);
+            Debug.Log(DC_BLUE + "SPEECH.Utterance: " + iSpeechInput.Utterance + DC_END);
             ReminderData.Instance.DateChoice = iSpeechInput.Utterance;
             mListen++;
-            // TODO - Consistency test
+            if (iSpeechInput.Utterance.Length > 0)
+            {
+                ReminderData.Instance.DateChoice = ExtractDateFromSpeech(iSpeechInput.Utterance);
+                Debug.Log(DC_GREEN + "DATE IS: " + ReminderData.Instance.DateChoice + DC_END);
+            }
         }
 
         // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            if (mListen < 1 && string.IsNullOrEmpty(ReminderData.Instance.DateChoice)) {
-                if (!Buddy.Vocal.IsBusy) {
+            if (mListen < TRY_NUMBER && string.IsNullOrEmpty(ReminderData.Instance.DateChoice))
+            {
+                if (!Buddy.Vocal.IsBusy)
+                {
                     Buddy.Vocal.SayAndListen(Buddy.Resources.GetString("when"), mGrammar.Grammars);
                 }
             }
-            if (mListen >= TRY_NUMBER || Input.GetKeyDown(KeyCode.A))
+            else if (mListen >= TRY_NUMBER || Input.GetKeyDown(KeyCode.RightArrow))
                 Trigger("DateEntryState");
+
+            // Temporary reset to quick test
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+                ReminderData.Instance.DateChoice = "";
         }
 
         // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -53,14 +73,146 @@ namespace BuddyApp.Reminder
             Buddy.Vocal.Stop();
         }
 
-        // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        //
-        //}
+        //  ---- PARSING FUNCTION ---- 
 
-        // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        //
-        //}
+        private string ExtractDateFromSpeech(string iSpeech)
+        {
+            DateTime lDate;
+            string lResult = null;
+            string[] lSpeechWords = iSpeech.Split(' ');
+            int lDayNum = 0;
+            int lMonth = DateTime.Today.Month;
+            int lYear = DateTime.Today.Year;
+
+            // One word in the speech
+            if (lSpeechWords.Length == 1)
+            {
+                if (ContainOneOfPhonetics(lSpeechWords, "today"))
+                    return DateTime.Today.ToString(DATE_FORMAT);
+                else if (ContainOneOfPhonetics(lSpeechWords, "tomorrow"))
+                    return DateTime.Today.AddDays(1F).ToString(DATE_FORMAT);
+                else if (ContainOneOfPhonetics(lSpeechWords, "weekdays"))
+                    return GetNextDay(DateTime.Today, lSpeechWords);
+            }
+            if (ContainOneOfPhonetics(lSpeechWords, "dayaftertomorrow"))
+                return DateTime.Today.AddDays(2F).ToString(DATE_FORMAT);
+            else if (ContainOneOfPhonetics(lSpeechWords, "weekdays") || ContainOneOfPhonetics(lSpeechWords, "the"))
+            {
+                // Try to build a date - Find day / number, month,
+                // If number or month is absent - check if "next" is present -> GetNextDay
+                // Find a day number
+                foreach (string lWord in lSpeechWords)
+                {
+                    if (int.TryParse(lWord, out lDayNum))
+                        break;
+                }
+                // If the day number is valid and a month is choose, create a date
+                if (lDayNum >= 1 && lDayNum <= 31 && ContainOneOfPhonetics(lSpeechWords, "month"))
+                {
+                    // Find the choosen month - If an error occured, the date extraction is stopped.
+                    if ((lMonth = FindMonthNumber(lSpeechWords)) < 0)
+                        return "";
+                    // If the month number is passed OR if the day is passed and the choosen month is the current month, increment the year.
+                    if (lMonth < DateTime.Today.Month || (lMonth == DateTime.Today.Month && lDayNum < DateTime.Today.Day))
+                        lYear++;
+                    lDate = new DateTime(lYear, lMonth, lDayNum);
+                    lResult = lDate.ToString(DATE_FORMAT);
+                }
+                // If the day number is valid but without month, we create a date to the next day number.
+                else if (lDayNum >= 1 && lDayNum <= 31)
+                {
+                    // If the day number is passed, use the next month
+                    if (lDayNum < DateTime.Today.Day)
+                        lMonth++;
+                    // Set to january if we were in december - Happy new year !
+                    if (lMonth > DECEMBER)
+                    {
+                        lMonth = JANUARY;
+                        lYear++;
+                    }
+                    lDate = new DateTime(lYear, lMonth, lDayNum);
+                    lResult = lDate.ToString(DATE_FORMAT);
+                }
+                // If no number is choose => speech = "the" + "next" ([TODO] Make this impossible with the grammar) || "weekdays" + "next"
+                else if (lDayNum == 0 && ContainOneOfPhonetics(lSpeechWords, "next"))
+                    return GetNextDay(DateTime.Today, lSpeechWords);
+            }
+            return lResult;
+        }
+
+        /*
+         *  Return the number of the first month found in iSpeech.
+         *  If no month are found, negative value is return.
+         */
+        private int FindMonthNumber(string[] iSpeech)
+        {
+            string[] lMonthArray = Buddy.Resources.GetPhoneticStrings("month");
+
+            foreach (string lWord in iSpeech)
+            {
+                for (int lIndex = 0; lIndex < lMonthArray.Length; lIndex++)
+                {
+                    if (lMonthArray[lIndex] == lWord)
+                        return lIndex;
+                }
+            }
+            return -1;
+        }
+
+        /*
+         *  Return the date in string format, of the next day, from a start day.
+         *  (It takes the first encounter day in the array)
+         *  If an error occured, a negative value is return.
+         */
+        private string GetNextDay(DateTime iStartDay, string[] iSpeech)
+        {
+            bool lFind = false;
+            string[] lWeekDays = Buddy.Resources.GetPhoneticStrings("weekdays");
+            int lNumDay = -1;
+            int lDaysToAdd = 0;
+
+            // Find the first day in the array
+            foreach (string lWord in iSpeech)
+            {
+                for (int lIndex = 0; lIndex < lWeekDays.Length; lIndex++)
+                {
+                    if (lWeekDays[lIndex] == lWord)
+                    {
+                        lNumDay = lIndex;
+                        lFind = true;
+                        break;
+                    }
+                }
+                if (lFind)
+                    break;
+            }
+            if (lNumDay < 0)
+                return null;
+            // Substract the choosen day number with the current day number
+            // The calcul assure the range is between [1-7]
+            lDaysToAdd = ((lNumDay - (int)iStartDay.DayOfWeek + 7) % 7) + 1;
+            return iStartDay.AddDays(lDaysToAdd).ToString(DATE_FORMAT);
+        }
+
+        /*
+         *  Return true if, a word in the phonetics string list represent by iKey (Dictionnary),
+         *  is present in the array of string iWords. 
+         */
+        private bool ContainOneOfPhonetics(string[] iWords, string iKey)
+        {
+            string[] lKeyWords = Buddy.Resources.GetPhoneticStrings(iKey);
+
+            for (int lIndex = 0; lIndex < iWords.Length; lIndex++)
+            {
+                foreach (string lWord in lKeyWords)
+                {
+                    Debug.Log(DC_RED + lWord + DC_END);
+                    if (iWords[lIndex].ToLower() == lWord.ToLower())
+                        return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
