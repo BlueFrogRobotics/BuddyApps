dd628c217831cff224290182f326a924a10c08b2
mc <mc@bluefrogrobotics.com>
2018-10-10 10:32:50 +0200

    [TakePhoto] addition of state Quit

---
+++ Assets/Apps/TakePhoto/Scripts/Behaviour/Quit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BuddyApp.TakePhoto
+{
+    public class Quit : AStateMachineBehaviour
+    {
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        public override void OnStateEnter(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
+        {
+            QuitApp();
+        }
+    }
+}
+
