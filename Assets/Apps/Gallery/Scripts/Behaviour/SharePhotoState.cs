a26979eee501ae6e5796a17e8a4850b6428a1999
Jordan Martin <jmartin@bluefrogrobotics.com>
2018-11-13 16:00:33 +0100

    [APP][GALLERY] Code cleaned

--- Assets/Apps/Gallery/Scripts/Behaviour/SharePhotoState.cs
+++ Assets/Apps/Gallery/Scripts/Behaviour/SharePhotoState.cs
@@ -10,112 +10,58 @@ namespace BuddyApp.Gallery
 {
     public class SharePhotoState : AStateMachineBehaviour
     {
-        private string mToken;
-        private string mTokenSecret;
-        private string mConsumerKey;
-        private string mConsumerSecret;
-        private string mAllHashtag;
-        private Publish mWhereToPublish;
-        private string[] mTweetMsg;
-        private EMail mMail;
-        private bool mUseKey;
-        
-        private Texture2D mTexture;
+        private readonly string STR_TWEET_TEXT = "tweet";
+        private readonly string STR_MAIL_SUBJECT = "emailsubject";
+        private readonly string STR_MAIL_TEXT = "emailtext";
+        private readonly string STR_TWITTER_ERROR = "errorconnexion";
+        private readonly string STR_SHARED = "pictureshared";
 
         private XMLData mXMLData;
 
         // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            mXMLData = new XMLData();
-            mMail = new EMail();
-            mTexture = new Texture2D(1, 1);
+            ExtLog.I(ExtLogModule.APP, GetType(), LogStatus.START, LogInfo.LOADING, "On State Enter...");
+
             mXMLData = Utils.UnserializeXML<XMLData>(Buddy.Resources.GetRawFullPath("Share/config.xml"));
-
-            mToken = mXMLData.Token;
-            mTokenSecret = mXMLData.TokenSecret;
-            mConsumerKey = mXMLData.ConsumerKey;
-            mConsumerSecret = mXMLData.ConsumerSecret;
-            mUseKey = mXMLData.UseKey;
-            mAllHashtag = mXMLData.TwitterHashtag;
-            mTweetMsg = mXMLData.TwitterText.Split('/');
-            mWhereToPublish = mXMLData.WhereToPublish;
-
-            mMail.Addresses.Clear();
-            string lMessage;
-            lMessage = mTweetMsg[Random.Range(0, mTweetMsg.Length)];
-            lMessage += " " + mAllHashtag;
-
-            //Buddy.GUI.Notifier.Display<SimpleNotification>().With(mAllHashtag, Buddy.Resources.Get<Sprite>("Ico_Twitter"));
-
-            switch(mWhereToPublish)
+            
+            if (null == mXMLData)
+            {
+                ExtLog.E(ExtLogModule.APP, GetType(), LogStatus.FAILURE, LogInfo.NULL_VALUE, "Failed to read configuration file.");
+                Buddy.Vocal.SayKey(STR_TWITTER_ERROR);
+                Trigger("TRIGGER_PHOTO_SHARED");
+                return;
+            }
+            
+            switch (mXMLData.WhereToPublish)
             {
                 case Publish.MAIL:
                     SendMail();
-                    break;
+                    return;
 
                 case Publish.TWITTER:
-                    SendTweet(lMessage);
-                    break;
+                    SendTweet();
+                    return;
 
                 case Publish.BOTH:
                     SendMail();
-                    SendTweet(lMessage);
-                    break;
+                    SendTweet();
+                    return;
             }
-
-            //Buddy.Vocal.SayKey("tweetpublished", true);
-            //Buddy.Vocal.Say(mAllHashtag, true);
-
+            
             Trigger("TRIGGER_PHOTO_SHARED");
         }
-
-
-
-        private void SendTweet(string iMsg)
-        {
-            Twitter.AccessTokenResponse accessToken = new Twitter.AccessTokenResponse {
-                Token = mToken,
-                TokenSecret = mTokenSecret
-            };
-
-            StartCoroutine(Twitter.API.UploadMedia(LoadTexture(mTexture), iMsg, mConsumerKey, mConsumerSecret, accessToken,
-                                                     new Twitter.PostTweetCallback(this.OnPostTweet)));
-        }
-
-        private void SendMail()
-        {
-            string lAdress = mXMLData.AdressMailSender;
-            string lPasswordMail = mXMLData.PasswordMail;
-            mMail.AddTexture2D(LoadTexture(mTexture), PhotoManager.GetInstance().GetCurrentPhoto().GetFullPath());
-            mMail.Addresses.Add(mXMLData.AdressMailReceiver);
-            mMail.Subject = mXMLData.SubjectMail;
-            mMail.Body = mXMLData.BodyMail;
-            Buddy.WebServices.EMailSender.Send(lAdress, lPasswordMail, SMTP.GMAIL, mMail);
-        }
-
-        void OnPostTweet(bool success)
-        {
-            Debug.Log("OnPostTweet - " + (success ? "succeeded." : "failed."));
-        }
-
-        private Texture2D LoadTexture(Texture2D iText)
-        {
-            byte[] bytes = new byte[File.ReadAllBytes(PhotoManager.GetInstance().GetCurrentPhoto().GetFullPath()).Length];
-            bytes = File.ReadAllBytes(PhotoManager.GetInstance().GetCurrentPhoto().GetFullPath());
-            iText.LoadImage(bytes);
-            return iText;
-        }
-
-
+        
         // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
             
         }
 
         // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            ExtLog.I(ExtLogModule.APP, GetType(), LogStatus.START, LogInfo.STOPPING, "On State Exit...");
         }
 
         // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
@@ -127,5 +73,72 @@ namespace BuddyApp.Gallery
         //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         //
         //}
+        
+        private void SendTweet()
+        {
+            Twitter.AccessTokenResponse accessToken = new Twitter.AccessTokenResponse {
+                Token = mXMLData.Token,
+                TokenSecret = mXMLData.TokenSecret
+            };
+
+            string strImageFullPath = PhotoManager.GetInstance().GetCurrentPhoto().GetPhotoFullpath();
+            FileInfo f = new FileInfo(strImageFullPath);
+            string strImageFileName = f.Name + f.Extension;
+
+            string[] mTweetMsg = null;
+            if (!string.IsNullOrEmpty(mXMLData.TwitterText)) {
+                mTweetMsg = mXMLData.TwitterText.Split('/');
+            }
+
+            string strTweetMsg = (null == mTweetMsg)
+                ? Buddy.Resources.GetRandomString(STR_TWEET_TEXT)
+                : mTweetMsg[Random.Range(0, mTweetMsg.Length)];
+            strTweetMsg += " " + mXMLData.TwitterHashtag;
+
+
+            byte[] image = File.ReadAllBytes(strImageFullPath);
+            ExtLog.W(ExtLogModule.APP, GetType(), LogStatus.FAILURE, LogInfo.NULL_VALUE, "Size of file to send : " + image.Length);
+
+            StartCoroutine(Twitter.API.UploadMedia(strImageFileName, image, strTweetMsg, mXMLData.ConsumerKey, mXMLData.ConsumerSecret, accessToken,
+                                                     new Twitter.PostTweetCallback(OnPostTweet)));
+        }
+
+        private void SendMail()
+        {
+            EMail mail = new EMail();
+            mail.Addresses.Clear();
+            mail.Addresses.Add(mXMLData.AdressMailReceiver);
+            mail.Subject = string.IsNullOrEmpty(mXMLData.SubjectMail) ? Buddy.Resources.GetRandomString(STR_MAIL_SUBJECT) : mXMLData.SubjectMail;
+            mail.Body = string.IsNullOrEmpty(mXMLData.BodyMail) ? Buddy.Resources.GetRandomString(STR_MAIL_TEXT) : mXMLData.BodyMail;
+            mail.AddFile(PhotoManager.GetInstance().GetCurrentPhoto().GetPhotoFullpath());
+            Buddy.WebServices.EMailSender.Send(mXMLData.AdressMailSender, mXMLData.PasswordMail, SMTP.GMAIL, mail, OnPostEmail);
+        }
+
+        void OnPostTweet(bool iSuccess)
+        {
+            if (!iSuccess) {
+                Buddy.Vocal.SayKey(STR_TWITTER_ERROR);
+            } else {
+                Buddy.Vocal.SayKey(STR_SHARED);
+            }
+
+            Trigger("TRIGGER_PHOTO_SHARED");
+        }
+
+        void OnPostEmail(bool iSuccess)
+        {
+            if (!iSuccess) {
+                Buddy.Vocal.SayKey(STR_TWITTER_ERROR);
+            }
+            else
+            {
+                if (Publish.MAIL == mXMLData.WhereToPublish)
+                {
+                    Buddy.Vocal.SayKey(STR_SHARED);
+                    Trigger("TRIGGER_PHOTO_SHARED");
+                }
+            }
+        }
+
     }
 }
