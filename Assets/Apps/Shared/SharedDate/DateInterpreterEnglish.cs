4993b1ab50a251e829d1e2514003c5066fdea7d5
Jordan Martin <jmartin@bluefrogrobotics.com>
2019-01-08 14:28:18 +0100

    [SHARED] Remove dead code.

--- Assets/Apps/Shared/SharedDate/DateInterpreterEnglish.cs
+++ Assets/Apps/Shared/SharedDate/DateInterpreterEnglish.cs
@@ -47,17 +47,6 @@ namespace BuddyApp.Shared
             thirtyfirst = 31
         }
 
-        private enum EDayOfWeek
-        {
-            monday = 0,
-            tuesday = 1,
-            wednesday = 2,
-            thursday = 3,
-            friday = 4,
-            saturday = 5,
-            sunday = 6
-        }
-
         private enum EMonths
         {
             january = 1,
