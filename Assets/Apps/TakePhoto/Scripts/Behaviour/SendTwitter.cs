a5f859eda865a835b4aa808fd5600a8ff1aed801
Delphine Lefebvre <dl@bluefrogrobotics.com>
2019-07-23 15:53:37 +0200

    [TAKEPHOTO] Add vocal msg when picture cannot be shared

--- Assets/Apps/TakePhoto/Scripts/Behaviour/SendTwitter.cs
+++ Assets/Apps/TakePhoto/Scripts/Behaviour/SendTwitter.cs
@@ -25,6 +25,8 @@ namespace BuddyApp.TakePhoto
 		private bool mMailOrTweetSent;
 		private bool mIsVocalSaid;
 
+        private bool mSuccess;
+
 		public override void Start()
 		{
 			mMailOrTweetSent = false;
@@ -33,56 +35,87 @@ namespace BuddyApp.TakePhoto
 			mXMLData = null;
 			mMail = new EMail();
 			mTexture = new Texture2D(1, 1);
+            mTweetMsg = null;
+            mSuccess = false;
 
-			if (File.Exists(Buddy.Platform.Application.PersistentDataPath + "Shared/config.xml")) {
+            string configFileName = Buddy.Platform.Application.PersistentDataPath + "Shared/config.xml";
+            if (!File.Exists(configFileName))
+            {
+                // Persistent file doesn't exist, try local configuration
+                Debug.Log("TakePhoto SendTwitter::Start Config file doesn't exist " + configFileName);
+                configFileName = Buddy.Resources.GetRawFullPath("Twitter/config.xml");
+                if (!File.Exists(configFileName))
+                {
+                    Debug.Log("TakePhoto SendTwitter::Start Config file doesn't exist " + configFileName + " ");
+                    return;
+                }
+            }
 
-				mXMLData = new XMLData();
-				mXMLData = Utils.UnserializeXML<XMLData>(Buddy.Platform.Application.PersistentDataPath + "Shared/config.xml");
 
-				mToken = mXMLData.Token;
-				mTokenSecret = mXMLData.TokenSecret;
-				mConsumerKey = mXMLData.ConsumerKey;
-				mConsumerSecret = mXMLData.ConsumerSecret;
-				mUseKey = mXMLData.UseKey;
-				mAllHashtag = mXMLData.TwitterHashtag;
-				mTweetMsg = mXMLData.TwitterText.Split('/');
-				mWhereToPublish = mXMLData.WhereToPublish;
+            mXMLData = Utils.UnserializeXML<XMLData>(configFileName);
 
-			}
+            if (mXMLData != null)
+            {
+                mToken = mXMLData.Token;
+                mTokenSecret = mXMLData.TokenSecret;
+                mConsumerKey = mXMLData.ConsumerKey;
+                mConsumerSecret = mXMLData.ConsumerSecret;
+                mUseKey = mXMLData.UseKey;
+                mAllHashtag = mXMLData.TwitterHashtag;
+                if (!string.IsNullOrEmpty(mXMLData.TwitterText))
+                    mTweetMsg = mXMLData.TwitterText.Split('/');
+                mWhereToPublish = mXMLData.WhereToPublish;
+            }
 		}
 
 		// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 		public override void OnStateEnter(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
 		{
+            bool success = false;
 			if (mXMLData != null) {
 				mMail.Addresses.Clear();
-				string lMessage;
-				lMessage = mTweetMsg[Random.Range(0, mTweetMsg.Length)];
+				string lMessage = "";
+                if (mTweetMsg != null)
+				    lMessage = mTweetMsg[Random.Range(0, mTweetMsg.Length)];
 				lMessage += " " + mAllHashtag;
 				if (mWhereToPublish == Publish.MAIL) {
-					SendMail();
+                    success = SendMail();
 				} else if (mWhereToPublish == Publish.TWITTER) {
-					SendTweet(lMessage);
+                    success = SendTweet(lMessage);
 				} else {
-					SendMail();
-					SendTweet(lMessage);
+                    success = SendMail();
+                    success |= SendTweet(lMessage);
 				}
 			}
-			Trigger("AskPhotoAgain");
+            if (!success)
+            {
+                Buddy.Vocal.SayKey("errorconnexion");
+                Trigger("AskPhotoAgain");
+            }
 		}
 
 		public override void OnStateUpdate(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
 		{
-			if (mMailOrTweetSent && !mIsVocalSaid) {
-				mIsVocalSaid = true;
-				Buddy.Vocal.SayKey("pictureshared");
-			}
+			if (mMailOrTweetSent && !mIsVocalSaid)
+            {
+                mIsVocalSaid = true;
+                string keyMsg = mSuccess ? "pictureshared" : "errorconnexion";
+                Buddy.Vocal.SayKey(keyMsg, (iOutput) =>
+                {
+                    Trigger("AskPhotoAgain");
+                });
+               
+            }
 		}
 
-		private void SendTweet(string iMsg)
+		private bool SendTweet(string iMsg)
 		{
-
-			if (mXMLData != null) {
+			if (mXMLData != null
+                && !string.IsNullOrEmpty(mToken)
+                && !string.IsNullOrEmpty(mTokenSecret)
+                && !string.IsNullOrEmpty(mConsumerKey)
+                && !string.IsNullOrEmpty(mConsumerSecret))
+            {
 				Twitter.AccessTokenResponse accessToken = new Twitter.AccessTokenResponse {
 					Token = mToken,
 					TokenSecret = mTokenSecret
@@ -90,44 +123,50 @@ namespace BuddyApp.TakePhoto
 
 				StartCoroutine(Twitter.API.UploadMedia(LoadTexture(mTexture), iMsg, mConsumerKey, mConsumerSecret, accessToken,
 														 new Twitter.PostTweetCallback(this.OnPostTweet)));
-			}
 
-			// TODO inform if no tweeter
+                return true;
+			}
+            return false;
 		}
 
-		private void SendMail()
+		private bool SendMail()
 		{
-			if (mXMLData != null) {
-				string lAdress = mXMLData.AdressMailSender;
+			if (mXMLData != null
+                && !string.IsNullOrEmpty(mXMLData.AdressMailSender)
+                && !string.IsNullOrEmpty(mXMLData.PasswordMail)
+                && !string.IsNullOrEmpty(mXMLData.AdressMailReceiver))
+            { 
+                string lAdress = mXMLData.AdressMailSender;
 				string lPasswordMail = mXMLData.PasswordMail;
 				mMail.AddTexture2D(LoadTexture(mTexture), TakePhotoData.Instance.PhotoPath);
 				mMail.Addresses.Add(mXMLData.AdressMailReceiver);
 				mMail.Subject = mXMLData.SubjectMail;
 				mMail.Body = mXMLData.BodyMail;
 				Buddy.WebServices.EMailSender.Send(lAdress, lPasswordMail, SMTP.GMAIL, mMail, iInput => OnMailSent(iInput));
-			}
+                return true;
+            }
+            return false;
+        }
 
-			// TODO inform if no mail
-		}
-
-		private void OnMailSent(bool iInput)
-		{
-			if (iInput) {
-				mMailOrTweetSent = true;
-			}
-		}
+		private void OnMailSent(bool success)
+        {
+            Debug.Log("OnMailSent - " + (success ? "succeeded." : "failed."));
+            mSuccess = success;
+            mMailOrTweetSent = true;
+        }
 
 		private void OnPostTweet(bool success)
 		{
 			Debug.Log("OnPostTweet - " + (success ? "succeeded." : "failed."));
-			mMailOrTweetSent = true;
+            mSuccess = success;
+            mMailOrTweetSent = true;
 		}
 
 		private Texture2D LoadTexture(Texture2D iText)
 		{
-			byte[] bytes = new byte[File.ReadAllBytes(TakePhotoData.Instance.PhotoPath).Length];
-			bytes = File.ReadAllBytes(TakePhotoData.Instance.PhotoPath);
-			iText.LoadImage(bytes);
+            byte[] bytes = new byte[File.ReadAllBytes(TakePhotoData.Instance.PhotoPath).Length];
+            bytes = File.ReadAllBytes(TakePhotoData.Instance.PhotoPath);
+            iText.LoadImage(bytes);
 			return iText;
 		}
 	}
