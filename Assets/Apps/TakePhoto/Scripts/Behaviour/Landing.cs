64f5a40b08b91936bb81debefcb3ed87ddd20292
GregoireMilliez <gm@bluefrogrobotics.com>
2019-10-24 12:00:11 +0200

    [TAKEPHOTO] Adding findHuman state

--- Assets/Apps/TakePhoto/Scripts/Behaviour/Landing.cs
+++ Assets/Apps/TakePhoto/Scripts/Behaviour/Landing.cs
@@ -6,30 +6,25 @@ namespace BuddyApp.TakePhoto
 {
     public sealed class Landing : AStateMachineBehaviour
     {
-        private float mTimer;
 
         public override void OnStateEnter(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
         {
-            mTimer = 0F;
-            ActionStartApp();
+            Buddy.Actuators.Head.SetPosition(15F, 0F);
+
             //Set the recognition threshold for the app
             Buddy.Vocal.DefaultInputParameters = new SpeechInputParameters();
             Buddy.Vocal.DefaultInputParameters.RecognitionThreshold = 6000;
+            Trigger("LookForUser");
 
         }
 
-        public override void OnStateUpdate(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
-        {
-            mTimer += Time.deltaTime;
-            //We also set a timer if we have a bug with the Buddy.Vocal.IsBusy because we still work on the vocal and we want to avoid being blocked here.
-            if(!Buddy.Vocal.IsBusy || mTimer > 6F)
-                Trigger("Photo");
-        }
+        //public override void OnStateUpdate(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
+        //{
+        //}
 
-        private void ActionStartApp()
-        {
-            //Buddy.Vocal.SayKey("movehands");
-            Buddy.Actuators.Head.SetPosition(20F, 0F, 75F);
-        }
+        //private void ActionStartApp()
+        //{
+        //    //Buddy.Vocal.SayKey("movehands");
+        //}
     }
 }
---
+++ Assets/Apps/TakePhoto/Scripts/Behaviour/LookForUser.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using BlueQuark;
+using System;
+
+namespace BuddyApp.TakePhoto
+{
+    public sealed class LookForUser : AStateMachineBehaviour
+    {
+        private float mTimer;
+
+
+        public override void OnStateEnter(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
+        {
+            mTimer = 0F;
+
+            Buddy.Perception.HumanDetector.OnDetect.Add(HumanDetected);
+        }
+
+        private void HumanDetected(HumanEntity[] iHumanEntities)
+        {
+            if (iHumanEntities.Length > 0) {
+                Buddy.Perception.HumanDetector.OnDetect.Remove(HumanDetected);
+                mTimer = 0F;
+
+                // Get the biggest area
+                double lMaxArea = 0;
+                double lMeanX = 0;
+                double lMeanY = 0;
+                int lNbHumans = 1;
+
+                if (iHumanEntities.Length == 1) {
+                    lMeanX += (iHumanEntities[0].Center.x / Buddy.Sensors.RGBCamera.Width);
+                    lMeanY += (iHumanEntities[0].Center.y + iHumanEntities[0].BoundingBox.y) / (2 * Buddy.Sensors.RGBCamera.Height);
+                } else {
+
+                    lNbHumans = 0;
+
+                    for (int i = 0; i < iHumanEntities.Length; ++i) {
+                        if (iHumanEntities[i].BoundingBox.area() > lMaxArea)
+                            lMaxArea = iHumanEntities[i].BoundingBox.area();
+                    }
+
+                    // Get centered position of closest humans
+                    for (int i = 0; i < iHumanEntities.Length; ++i) {
+                        if (iHumanEntities[i].BoundingBox.area() * 1.3 > lMaxArea) {
+                            lMeanX += (iHumanEntities[i].Center.x / Buddy.Sensors.RGBCamera.Width);
+                            lMeanY += (iHumanEntities[i].Center.y + iHumanEntities[i].BoundingBox.y) / (2 * Buddy.Sensors.RGBCamera.Height);
+                            lNbHumans++;
+                        }
+                    }
+                }
+
+                lMeanX /= lNbHumans;
+                lMeanY /= lNbHumans;
+
+
+                float lNoAngle = Buddy.Actuators.Head.No.Angle + ((float)lMeanX - 0.5F) * 60F;
+                float lYesAngle = Buddy.Actuators.Head.Yes.Angle + ((float)lMeanY - 0.5F) * 49.5F;
+                Buddy.Actuators.Head.SetPosition((float)lMeanX, (float)lMeanY);
+                Trigger("Photo");
+
+            }
+        }
+
+        public override void OnStateUpdate(Animator iAnimator, AnimatorStateInfo iStateInfo, int iLayerIndex)
+        {
+            mTimer += Time.deltaTime;
+
+            if (mTimer > 2F)
+                Trigger("Photo");
+
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            Buddy.Perception.HumanDetector.OnDetect.Remove(HumanDetected);
+        }
+    }
+}
