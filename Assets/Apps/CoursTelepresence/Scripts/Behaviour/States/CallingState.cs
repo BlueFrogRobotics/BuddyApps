9070cf36d71ef72cd199fd108beacb9d10981377
GregoireMilliez <gregoire@milliez.fr>
2020-04-17 13:03:34 +0200

    [COURSTELEPRESENCE] Adding animator + states

---
+++ Assets/Apps/CoursTelepresence/Scripts/Behaviour/States/CallingState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using BlueQuark;
+using System;
+
+namespace BuddyApp.CoursTelepresence
+{
+
+    public sealed class CallingState : AStateMachineBehaviour
+    {
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            Debug.Log("calling state");
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+
+        //}
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            Debug.Log("calling state exit");
+        }
+    }
+
+}
---
+++ Assets/Apps/CoursTelepresence/Scripts/Behaviour/States/IdleState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using BlueQuark;
+using System;
+
+namespace BuddyApp.CoursTelepresence
+{
+
+    public sealed class IdleState : AStateMachineBehaviour
+    {
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            Debug.Log("Idle state");
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+
+        //}
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            Debug.Log("Idle state exit");
+        }
+    }
+
+}
