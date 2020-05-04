b78c19a88f0f09b9770698238a74a57732735fc1
GregoireMilliez <gregoire@milliez.fr>
2020-05-04 15:14:04 +0200

    [COURSTELEPRESENCE] Adding safety on callbacks

--- Assets/Apps/CoursTelepresence/Scripts/Behaviour/States/IdleState.cs
+++ Assets/Apps/CoursTelepresence/Scripts/Behaviour/States/IdleState.cs
@@ -21,9 +21,16 @@ namespace BuddyApp.CoursTelepresence
             mRTMManager = GetComponent<RTMManager>();
             mRTCManager = GetComponent<RTCManager>();
 
+            mCallButton = GetGameObject(10).GetComponentInChildren<Button>();
+        }
+
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            Debug.Log("Idle state");
+
             mRTMManager.OncallRequest = (CallRequest lCall) => { Trigger("INCOMING CALL"); };
 
-            mCallButton = GetGameObject(10).GetComponentInChildren<Button>();
             mCallButton.onClick.AddListener(() => {
                 Debug.LogWarning("Join channel " + mChannelId + " waiting for tablet answer");
                 Trigger("CALLING");
@@ -31,19 +38,13 @@ namespace BuddyApp.CoursTelepresence
                 mRTMManager.RequestConnexion(mChannelId, Buddy.Platform.RobotUID);
             }
             );
-
-            
-        }
-        
-
-        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            Debug.Log("Idle state");
         }
 
         // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
         override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
+            mRTMManager.OncallRequest = null;
+            mCallButton.onClick.RemoveAllListeners();
             Debug.Log("Idle state exit");
         }
     }
